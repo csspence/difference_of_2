fe03ad9e76922a2e2add33ebb75bbe05d8d3c041
Crew Spence <csspence123@gmail.com>
2020-03-28 10:34:00 -0500

    Created function stub for Code Wars toy problem.

---
+++ difference_of_2.js
@@ -0,0 +1,17 @@
+/*
+The objective is to return all pairs of integers from a given collection of integers that have a difference of 2.
+
+The result should be sorted in ascending order.
+
+The input will consist of unique values. The order of the integers in the input collection should not matter.
+
+Examples
+[1, 2, 3, 4]      -->  [[1, 3], [2, 4]]
+[4, 1, 2, 3]      -->  [[1, 3], [2, 4]]
+[1, 23, 3, 4, 7]  -->  [[1, 3]]
+[4, 3, 1, 5, 6]   -->  [[1, 3], [3, 5], [4, 6]]
+*/
+
+function twosDifference(input){
+ //Enter your solution here
+}
